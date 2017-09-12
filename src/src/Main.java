8897feb0a2ae319e0ce2f7e5c61a117b0e8938fb
Naga Mounika Kalidindi <mounikak637@gmail.com>
2017-09-12 02:19:38 -0700

    3rd

---
+++ src/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+		StudentGroup sg = new StudentGroup(3);
+		/*Date date = null;
+		try
+		{
+			String valuee="2017-09-12";
+			date =new SimpleDateFormat("yyyy-mm-dd").parse(valuee);
+			//System.out.println("Date is ::"+ date);
+		}
+		catch(Exception e){
+            System.out.println("Error::"+e);
+            e.printStackTrace();
+        }
+		
+		Student s = new Student(501, "K Mouni", date, 250.50);
+		
+		sg.addLast(s);
+		/*Student[] st = sg.getStudents();
+		for (int i = 0; st[i] != null ; i++)
+		{
+			System.out.println(st[i] + "****");
+		}*/
+
+	}
+
+}
